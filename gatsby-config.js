0b34895e50c15c515982ccb6ea83dc2272ba46e7
faizcaster <59181426+faizcaster@users.noreply.github.com>
2022-05-17 09:41:18 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Mutiakun Pages`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Mutiakun`,
+      summary: `no bio yet.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `No bio yet`,
+    siteUrl: `https://mutiakun.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `mutiakun`,
     },
   },
   plugins: [
